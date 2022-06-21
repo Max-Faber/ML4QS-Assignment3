252ba5ac0478b9ada8daffca1cf9653e902da8f6
b0ster <bosterenborg@gmail.com>
2022-06-21 12:17:37 +0200

    types fixed

--- trainTestValSplit.py
+++ trainTestValSplit.py
@@ -5,16 +5,20 @@ from os.path import isfile, join
 from os import listdir
 from sklearn.model_selection import train_test_split
 
-def getCSVs(_dataset_path: str) -> set[str, pd.DataFrame]:
-    paths_CSV: set[str] = set([join(_dataset_path, f) for f in listdir(_dataset_path) if isfile(join(_dataset_path, f))])
+
+def getCSVs(_dataset_path: str) -> dict[str, pd.DataFrame]:
+    paths_CSV: set[str] = set(
+        [join(_dataset_path, f) for f in listdir(_dataset_path) if isfile(join(_dataset_path, f))])
     return {path_CSV: pd.read_csv(path_CSV) for path_CSV in paths_CSV}
 
 
-def getTrainTestValSplit(_dataset_path: str, _train_frac: float = 0.7, _test_frac: float = 0.2, _val_frac: float = 0.1) -> list[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-    CSVs: set[str, pd.DataFrame] = getCSVs(_dataset_path=_dataset_path)
+def getTrainTestValSplit(_dataset_path: str, _train_frac: float = 0.7, _test_frac: float = 0.2,
+                         _val_frac: float = 0.1) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    CSVs: dict[str, pd.DataFrame] = getCSVs(_dataset_path=_dataset_path)
     assert _train_frac + _test_frac + _val_frac <= 1.0
 
-    train_filenames, test_filenames = train_test_split(list(CSVs.keys()), test_size=_test_frac, train_size=_train_frac, random_state=7342)
+    train_filenames, test_filenames = train_test_split(list(CSVs.keys()), test_size=_test_frac, train_size=_train_frac,
+                                                       random_state=7342)
     val_filenames = set(CSVs.keys()) - set(train_filenames) - set(test_filenames)
 
     train_CSVs: list[pd.DataFrame] = [CSVs[df] for df in train_filenames]
@@ -25,7 +29,7 @@ def getTrainTestValSplit(_dataset_path: str, _train_frac: float = 0.7, _test_fra
     test_concat: pd.DataFrame = pd.concat(test_CSVs)
     val_concat: pd.DataFrame = pd.DataFrame(val_CSVs)
 
-    del CSVs, train_CSVs, test_CSVs, val_CSVs # Release memory
+    del CSVs, train_CSVs, test_CSVs, val_CSVs  # Release memory
 
     return train_concat, test_concat, val_concat
 
