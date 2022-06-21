80499f576d50ce65d458b4006f3a57d96697fe29
maxfaber <maxfaber@quicknet.nl>
2022-06-21 13:58:30 +0200

    Added main activity dict with its corresponding class labels

--- definitions.py
+++ definitions.py
@@ -1,3 +1,12 @@
 import os
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
+ROOT_DIR: str = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
+main_activity_label_classes: dict[str, int] = {
+    'label:UNKNOWN': -1,
+    'label:LYING_DOWN': 0,
+    'label:SITTING': 1,
+    'label:OR_standing': 2,
+    'label:FIX_walking': 3,
+    'label:FIX_running': 4,
+    'label:BICYCLING': 5
+}
