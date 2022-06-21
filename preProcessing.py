909194f7360d0272790c97a38170e1eb7f6e6e32
maxfaber <maxfaber@quicknet.nl>
2022-06-21 13:06:46 +0200

    Signature of preProcessing.py (modified)

--- preProcessing.py
+++ preProcessing.py
@@ -3,12 +3,12 @@ import pandas as pd
 from definitions import ROOT_DIR
 from trainTestValSplit import getTrainTestValSplit
 
-def preProcess(_df: pd.DataFrame) -> pd.DataFrame:
+def preProcess(_df: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
     return _df
 
 if __name__ == '__main__':
     # lying down, sitting, standing in place, standing and moving, walking, running, bicycling.
-    main_activity_labels: list[str] = ['label:LYING_DOWN', 'label:SITTING', 'label:OR_standing', 'label:FIX_walking', '']
+    main_activity_labels: list[str] = ['label:LYING_DOWN', 'label:SITTING', 'label:OR_standing', 'label:FIX_walking', 'label:FIX_running', '']
     train, test, val = getTrainTestValSplit(_dataset_path=f'{ROOT_DIR}/dataset/ExtraSensory/ExtraSensory.per_uuid_features_labels')
     train = preProcess(_df=train)
     test = preProcess(_df=test)
