e0dbb464247717f61422bbb1794214f31f2ed3d2
maxfaber <maxfaber@quicknet.nl>
2022-06-21 13:04:20 +0200

    Signature of preProcessing.py

--- preProcessing.py
+++ preProcessing.py
@@ -1,8 +1,17 @@
 import pandas as pd
 
 from definitions import ROOT_DIR
+from trainTestValSplit import getTrainTestValSplit
+
+def preProcess(_df: pd.DataFrame) -> pd.DataFrame:
+    return _df
 
 if __name__ == '__main__':
-    df_uuid_test: pd.DataFrame = pd.read_csv(f'{ROOT_DIR}/datasets/ExtraSensory/ExtraSensory.per_uuid_features_labels/0A986513-7828-4D53-AA1F-E02D6DF9561B.features_labels.csv')
-    df_uuid_test.insert(1, 'datetime', pd.to_datetime(df_uuid_test['timestamp'], unit='s'))
-    pass
+    # lying down, sitting, standing in place, standing and moving, walking, running, bicycling.
+    main_activity_labels: list[str] = ['label:LYING_DOWN', 'label:SITTING', 'label:OR_standing', 'label:FIX_walking', '']
+    train, test, val = getTrainTestValSplit(_dataset_path=f'{ROOT_DIR}/dataset/ExtraSensory/ExtraSensory.per_uuid_features_labels')
+    train = preProcess(_df=train)
+    test = preProcess(_df=test)
+    val = preProcess(_df=val)
+    # df_uuid_test.insert(1, 'datetime', pd.to_datetime(df_uuid_test['timestamp'], unit='s'))
+    # pass
