e52a990c0065472c69d653d0b0d1a2d619d9d285
maxfaber <maxfaber@quicknet.nl>
2022-06-22 16:18:42 +0200

    Added mean metric for window output evaluation

--- definitions.py
+++ definitions.py
@@ -2,11 +2,11 @@ import os
 
 ROOT_DIR: str = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
 main_activity_label_classes: dict[str, int] = {
-    'label:UNKNOWN': -1,
-    'label:LYING_DOWN': 0,
-    'label:SITTING': 1,
-    'label:OR_standing': 2,
-    'label:FIX_walking': 3,
-    'label:FIX_running': 4,
-    'label:BICYCLING': 5
+    'label:UNKNOWN': 0,
+    'label:LYING_DOWN': 1,
+    'label:SITTING': 2,
+    'label:OR_standing': 3,
+    'label:FIX_walking': 4,
+    'label:FIX_running': 5,
+    'label:BICYCLING': 6
 }
