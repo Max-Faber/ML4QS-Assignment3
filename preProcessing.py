8c5500870eddcc65151c70e770a729ff434057ca
maxfaber <maxfaber@quicknet.nl>
2022-06-24 12:37:38 +0200

    preProcessing.py now only utilizes the selected features

--- preProcessing.py
+++ preProcessing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from definitions import ROOT_DIR, main_activity_label_classes
+from definitions import ROOT_DIR, main_activity_label_classes, columns_to_keep
 from trainTestValSplit import getTrainTestValSplit
 from copy import deepcopy
 
@@ -24,6 +24,7 @@ def preProcess(_df: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
     _df: pd.DataFrame = addClassLabels(_df=_df)
     _df: pd.DataFrame = removeLabels(_df=_df)
     _df: pd.DataFrame = _df.drop(columns=['timestamp'])
+    _df: pd.DataFrame = _df[columns_to_keep]
     y: pd.Series = _df['target']
     x: pd.DataFrame = _df.drop(columns=['target'])
     del _df
